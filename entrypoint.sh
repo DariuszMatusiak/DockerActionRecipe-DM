04c0e3a468198409c87d827960b2e4cfc7eaf003
Dariusz Matusiak <221199402+DariuszMatusiak@users.noreply.github.com>
2025-08-07 12:19:10 +0200

    Changes 009

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "Running in a Docker container"
 echo "Current directory: $(pwd)"
 echo "Files in the current directory:"
 ls -la
-echo "answer1=423" >> $GITHUB_OUTPUT
+echo "answer1=42" >> $GITHUB_OUTPUT
 echo "### hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
 echo "<h3> The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
